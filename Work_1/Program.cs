7f3a0de3d027d12b7f4fbf0dcecbdfadc48c55d3
BohdanBoss <bogdanburduluk@gmail.com>
2022-07-13 12:11:11 +0300

    Added Work 1

---
+++ Work_1/Program.cs
@@ -0,0 +1,72 @@
+﻿// Task 1:
+int num1;
+int num2;
+Console.Write("Input first intenger number ");
+num1=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input srcond intenger number ");
+num2=Convert.ToInt32(Console.ReadLine());
+
+if(num1<num2)
+   {
+   Console.WriteLine("Чисоло num1 минимальное ");
+   Console.WriteLine("Число num2 максимальное ");
+   }
+if(num1>num2)   
+   {
+   Console.WriteLine("Число num1 максимальное ");
+   Console.WriteLine("Число num2 минимальное ");
+   }
+
+/* Task 2
+Console.Write("Input first intenger number ");
+int num1=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input srcond intenger number ");
+int num2=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input third intenger number ");
+int num3=Convert.ToInt32(Console.ReadLine());
+int max=num1;
+if(max<num2)
+   {
+   Console.Write("Max number is " +num2 );
+   }
+else
+   {
+   Console.Write("Max number is " +num1 );
+   } 
+if(max<num3)
+   {
+      Console.Write("Max number is " +num3 );
+   }
+   */
+/* Task 3
+Console.Write("Input intenger number ");
+int number=Convert.ToInt32(Console.ReadLine());
+
+if (number%2==0)
+   {
+   Console.Write("Even ");
+   }
+else
+   {
+      Console.Write("Uneven ");
+   }
+   */
+
+/* Task 4   
+Console.Write("Input interger number ");
+int num=Convert.ToInt32(Console.ReadLine());
+int current =1;
+
+while(current <= num)
+     
+ {
+   if(current %2==0)
+   Console.Write(current + " ");
+   current++;
+ }
+ */
+ 
+
