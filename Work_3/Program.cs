341e7d638436e3f89fbd2d8f1547f8cb001ea62f
BohdanBoss <bogdanburduluk@gmail.com>
2022-07-16 19:58:38 +0300

    Added Wokr 3

---
+++ Work_3/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Task 1
+bool IsPolendrom(int number)
+{
+
+    if((number/1000)==((number%10)*10) + (number%100/10)) return true;
+    else 
+    return false;
+}
+
+
+Console.WriteLine("Ввидите пятизначное число ");
+int number=Convert.ToInt32(Console.ReadLine());
+bool num=IsPolendrom(number);
+if(true)
+Console.WriteLine("Это число является полиндромом ");
+else
+Console.WriteLine("Это число не является полиндромо ");
+*/
+
+
