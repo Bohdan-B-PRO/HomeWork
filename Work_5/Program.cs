80959dc17e0e548cba74c2b39fde4aac094b78ee
BohdanBoss <bogdanburduluk@gmail.com>
2022-08-08 20:40:07 +0300

    Added info Work 5

---
+++ Work_5/Program.cs
@@ -0,0 +1,31 @@
+﻿// Task 1
+/*
+int[]GenerationRandomArray(int size, int minValue, int maxValue)  
+{
+    int[] newArray= new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+       newArray[i] = new Random().Next(minValue, maxValue + 1); 
+    }
+   
+    return newArray;
+}
+
+void PowerArray(int []array)
+{
+int result = 0;
+for (int i = 0; i < array.Length; i++)
+if (array[i] % 2 == 0)
+    result+=array[i];
+{
+Console.WriteLine($"Всего {array.Length} чисел,  сумма чётных {result} ");
+}
+
+}
+
+int[]array = GenerationRandomArray(4, 100, 1000);
+PowerArray(array);
+*/
+
+
