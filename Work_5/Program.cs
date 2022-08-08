f202dcd7806027555017b103cbe3f197b474179c
BohdanBoss <bogdanburduluk@gmail.com>
2022-08-08 21:31:21 +0300

    Added info Work 5

--- Work_5/Program.cs
+++ Work_5/Program.cs
@@ -28,4 +28,53 @@ int[]array = GenerationRandomArray(4, 100, 1000);
 PowerArray(array);
 */
 
+//Task 2
+int[]GenerationRandomArray(int size, int minValue, int maxValue)  
+{
+    int[] newArray= new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+       newArray[i] = new Random().Next(minValue, maxValue + 1); 
+    }
+   
+    return newArray;
+}
+
+void PowerArray(int []array)
+{
+int result = 0;
+for (int i = 0; i < array.Length; i++)
+if (array[i] % 2 != 0)
+    result+=array[i];
+{
+Console.WriteLine($"Всего {array.Length} чисел, из них сумма нечётных {result} ");
+}
+
+}
+
+int[]array = GenerationRandomArray(4, 100, 1000);
+PowerArray(array);
+
+
+
+
+//Task3
+/*
+int[] CreateArray( int[] array, int size, int min, int max) 
+
+{
+    int[] newArray= new int[size];
+    
+    for(int i = 0; i < size; i++)
+    {
+        if (array[i] > max) max = array[i];
+        if (array[i] < min) min = array[i];
+        Console.Write($"Input {i+1} element of {size}: ");
+        newArray[i]=Convert.ToInt32(Console.ReadLine());
+    }
+    return newArray;
+}
+
+*/
 
