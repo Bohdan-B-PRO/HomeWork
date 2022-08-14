c6ec4091885c22c94f3710a1eab8d17921002b37
BohdanBoss <bogdanburduluk@gmail.com>
2022-08-14 13:26:33 +0300

    Added info Work 6

--- Work_6/Program.cs
+++ Work_6/Program.cs
@@ -15,5 +15,26 @@ int m = Convert.ToInt32(Console.ReadLine());
 InputNumbers(m);
 Console.Write($"Количество чисел после нуля {m} ");
 */
+//Task 2
+void  LineIntersection( int b1, int k1, int b2, int k2)
+{
+  int x= 0;
+  int y = 0;
+  x = (b2 - b1) / (k1 - k2);
+  y = k1 * (b2 - b1) / (k1 - k2) + b1;
+ //Console.Write(x,y);
+}
 
+Console.WriteLine("Ввидите координаты b1 ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввидите координаты k1 ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввидите координаты b2 ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввидите координаты k2 ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+LineIntersection(b1, k1, b2, k2);
+
+
+Console.Write("Координаты пересечения точе x,y " + LineIntersection );
 
