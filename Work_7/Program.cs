d78550d8a59dbd9b6df6b1f7ac9dd35a8206d833
BohdanBoss <bogdanburduluk@gmail.com>
2022-08-15 17:29:06 +0300

    Added info Work 7

--- Work_7/Program.cs
+++ Work_7/Program.cs
@@ -1,4 +1,5 @@
 ﻿//Task 1
+/*
 int[,]CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
 {
     int [,]newArray = new int[rows, columns];
@@ -11,8 +12,6 @@ int[,]CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
     }
     return newArray;
 }
-
-
 
 void Show2dArray(int[,]array)
 {
@@ -38,4 +37,50 @@ int max = Convert.ToInt32(Console.ReadLine());
 
 int[,]array= CreateRandom2dArray(n,m, min, max);
 Show2dArray(array);
+*/
 
+int[,]CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int [,]newArray = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            newArray[i,j]= new Random().Next(minValue, maxValue +1);
+        }
+    }
+    return newArray;
+}
+
+void Show2dArray(int[,]array)
+
+{
+    for(int i = 0; i < array.GetLength(0); i++ )
+    {
+       for(int j = 0; j < array.GetLength(1); j++)
+      {
+        Console.Write(array[i,j] + " ");
+      }
+      Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min possible value of element: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max possible value of element: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,]array= CreateRandom2dArray(n,m,min, max);
+Show2dArray(array);
+
+Console.WriteLine($"Input interesting your element ");
+int Coord= Convert.ToInt32(Console.ReadLine());
+if (Coord > min && Coord > max)
+{
+    Console.WriteLine("Такого числа нет! ");
+}
