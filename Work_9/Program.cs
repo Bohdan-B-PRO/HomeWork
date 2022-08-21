a2a2fdc447ab3c812d0725fe5249b66243f40e35
BohdanBoss <bogdanburduluk@gmail.com>
2022-08-21 20:19:43 +0300

    Added info Work 9

---
+++ Work_9/Program.cs
@@ -0,0 +1,16 @@
+﻿//Task 1
+/*
+void ShowNumbers(int m, int n)
+{
+    if(n != m) ShowNumbers(m, n - 1);
+
+    Console.Write(n + " ");
+}
+Console.Write("Input number M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+ShowNumbers(m, n);
+*/
